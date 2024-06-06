43d679a90ec8ccc2ac27beadc86dce7c84287de8
Aseem Shaikh <88926673+AseemShaikh32@users.noreply.github.com>
2024-06-06 12:20:21 +0530

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('token')) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  }
+  return req;
+});
+
+export const login = (formData) => API.post('/login', formData);
+export const register = (formData) => API.post('/register', formData);
